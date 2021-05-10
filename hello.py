69d88eda2ddab13ae76b00832480fca49bc467a0
NikitaDem01 <55186967+NikitaDem01@users.noreply.github.com>
2021-05-11 01:40:28 +0300

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,78 +1,113 @@
-import numpy.random as rand
+a = "1234567890"
+b = "45"
 
-n = rand.randint(3, 10 ** 4)
-min_lim = 1
-max_lim = 10 ** 6
-matrix = rand.randint(min_lim, max_lim, n)
-# print(matrix)
-
-m = rand.randint(1, 100)
-min_li = 0
-max_li = 10 ** 9
-matri = rand.randint(min_li, max_li, m)
-# print(matri)
-
-k = rand.randint(1, 100)
-max_limm = 100
-matr = rand.randint(min_lim, max_limm, (k, m))
-# print(matr)
-
-def maxPerimeter(mas):
-    maxp = 0
-    n = len(mas)
-    for i in range(n - 2):
-        for j in range(i + 1, n - 1):
-            for k in range(j + 1, n):
-                a = mas[i]
-                b = mas[j]
-                c = mas[k]
-                if (a < b + c and b < a + c
-                        and c < a + b):
-                    maxp = max(maxp, a + b + c)
-            if (maxp == 0):
-                return "Создание треугольника невозможно"
+def prefics(s):
+    p = []
+    j = 0
+    i = 1
+    p.append(0)
+    while len(p) != len(s):
+        if s[i] == s[j]:
+            p.append(j + 1)
+            j += 1
+            i += 1
+        else:
+            if j == 0:
+                p.append(0)
+                i += 1
             else:
-                return "Максимальный периметр = " + str(maxp)
+                j = p[j - 1]
+    return p
 
-def maxNumber(nums):
-        strums = []
-        for i in nums:
-            strums.append(str(i))
-        result = []
-        startlen = len(strums)
-        while len(result) < startlen:
-            max = '0'
-            for i in strums:
-                if len(i) < len(max):
-                    if int(max) < int(i) * 10 ** (len(max) - len(i)):
-                        max = i
-                elif len(i) > len(max):
-                    if int(i) > int(max) * 10 ** (len(i) - len(max)):
-                        max = i
+def kmp(s, f):
+    p = prefics(s)
+    l = len(f)
+    counter = 0
+    i = 0
+    while len(s) > 0:
+        if s[i] == f[i]:
+            i += 1
+        else:
+            if i != 0:
+                s = s[1 + p[i - 1]:]
+                counter += 1 + p[i - 1]
+            else:
+                s = s[1:]
+                counter += 1
+            i = 0
+
+        if i == l:
+            return counter
+    return -1
+
+def gen_table(s):
+    r = s[::-1]
+    index_list = []
+    use_list = []
+    box = r
+    r = r[1:]
+    counter = 0
+    for i in r:
+        counter += 1
+        res = simple_find(use_list, i)
+        if res == -1:
+            index_list.append(counter)
+            use_list.append(i)
+        else:
+            index_list.append(res + 1)
+
+    index_list = index_list[::-1]
+    res = simple_find(use_list, box[0])
+
+    if res == -1:
+        index_list.append(len(r) + 1)
+        use_list = use_list[::-1]
+        use_list.append(box[0])
+        use_list = use_list[::-1]
+    else:
+        index_list.append(res + 1)
+
+    return index_list, use_list
+
+def bm(s, f):
+    index_list, use_list = gen_table(f)
+    last = len(f) - 1
+    global_counter = 0
+    while len(s) > 0:
+        if len(s) < len(f):
+            return -1
+        if f[last] != s[last]:
+            res = simple_find(f, s[last])
+            if res == -1:
+                s = s[index_list[-1]:]
+                global_counter += index_list[-1]
+            else:
+                s = s[index_list[res]:]
+                global_counter += index_list[res]
+
+        else:
+            counter = 0
+            for i in range(len(f)):
+                if f[i] == s[i]:
+                    counter += 1
                 else:
-                    if int(i) > int(max):
-                        max = i
-            result.append(max)
-            strums.remove(max)
-        resstr = ''
-        for i in result:
-            resstr = resstr + i
-        return resstr
+                    s = s[index_list[i]:]
+                    global_counter += index_list[i]
+                    break
+                if counter == last + 1:
+                    return global_counter
+    return -1
 
-def diagonalSort(nums):
-    # Устанавливаем swapped в True, чтобы цикл запустился хотя бы один раз
-    swapped = True
-    while swapped == True:
-        swapped = False
-        for i in range(len(nums) - 1):
-            for j in range(len(nums[0]) - 1):
-                if nums[i][j] > nums[i + 1][j + 1]:
-                    # Меняем элементы
-                    nums[i][j], nums[i + 1][j + 1] = nums[i + 1][j + 1], nums[i][j]
-                    # Устанавливаем swapped в True для следующей итерации
-                    swapped = True
+def simple_find(l, k):
+    index = -1
+    counter = 0
+    for i in l:
+        if i == k:
+            index = counter
+        counter += 1
+    return index
 
-print("1:", maxPerimeter(matrix))
-print("2:", maxNumber(matri))
-diagonalSort(matr)
-print("3:", matr)
+print("Алгоритм Кнута-Морриса-Пратта: " + str(kmp(a, b)))
+print("Алгоритм Бойера-Мура: " + str(bm(a, b)))
+x = a.find(b)
+print("Встроенный поиск(python): " + str(x))
